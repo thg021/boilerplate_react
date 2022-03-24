289b52b503527202897709fbd54f7086509ca317
Thiago Silva <thiago.silva@hotmail.com>
2022-03-24 12:19:31 -0300

    feat: add global styles

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,19 @@
+import * as S from './styles'
+
 const Main = () => (
-    <main>
-        <h1>react avançado</h1>
-    </main>
+    <S.Wrapper>
+        <S.Logo
+            src="/img/logo.svg"
+            alt="Imagem de um átomo e React Avançado escrito ao lado"
+        />
+        <S.Title>React avançado</S.Title>
+        <S.Description>
+            Typescript, ReactJs, NextJs e Styled Components
+        </S.Description>
+        <S.Illustration
+            src="/img/hero-illustration.svg"
+            alt="Imagem de um homem sentado na cadeira programando"
+        />
+    </S.Wrapper>
 )
 export default Main
