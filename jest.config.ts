d1c2af370caa524a0a3a13c1fd323ef5a86acf56
Thiago Silva <thiago.silva@hotmail.com>
2022-03-28 15:11:29 -0300

    chore: ignore test on stories

--- jest.config.ts
+++ jest.config.ts
@@ -20,7 +20,7 @@ export default {
     collectCoverage: true,
 
     // An array of glob patterns indicating a set of files for which coverage information should be collected
-    collectCoverageFrom: ['src/**/*.ts(x)'],
+    collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/*/stories.tsx'],
 
     // The directory where Jest should output its coverage files
     // coverageDirectory: undefined,
